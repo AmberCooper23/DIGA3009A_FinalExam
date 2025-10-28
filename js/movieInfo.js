c5bc8520b132f98f26c6cd7fec3ca1f4ffe7bd7b
AmberCooper23 <2677396@students.wits.ac.za>
2025-10-29 01:09:10 +0200

    styling maybe?

--- js/movieInfo.js
+++ js/movieInfo.js
@@ -20,6 +20,7 @@ async function loadMovie() {
         const container = document.getElementById('movieDetails');
         container.innerHTML = `
         <h1>${movie.title} (${new Date(movie.release_date).getFullYear()})</h1>
+        <div class="movieInfo">
         <img src = "${IMG_BASE}${movie.poster_path}" alt="${movie.title}" class = "filmList">
         <p> <strong> Director: </strong> ${director}<?p>
         <p> <strong> Main Cast: </strong> ${mainCast}</p>
