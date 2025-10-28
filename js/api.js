d05010a683be5fdb3f5b0ecc455f6291d2811655
AmberCooper23 <2677396@students.wits.ac.za>
2025-10-28 14:21:32 +0200

    setting up index.html

--- js/api.js
+++ js/api.js
@@ -1,3 +1,33 @@
 const API_KEY = '0cefd7764121a70764185523e70202ae';
-const BASE_URL = 'https//api.themoviedb.org/3';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const IMG_BASE = 'https://image.tmdb.org/t/p/w500';
 
+async function fetchFilms(endpoint, listId) {
+    try{
+
+  const response = await fetch('${BASE_URL}/${endpoint}?api_key=${API_KEY}&language=en-US&page=1');
+    const data = await response.json();
+
+    const list = document.querySelector(listId);
+
+    container.innerHTML = '';
+
+    data.results.slice(0,6).forEach(film=>{
+        const item = document.createElement('li');
+        const img = document.createElement('img');
+        const title = document.createElement('p');
+
+        img.src = '${IMG_BASE}${movie.poster_path}';
+        img.alt = movie.title;
+
+        list.append(img, title);
+        list.appendChild(li);
+    });
+
+} catch (err) {
+    console.error('Error fetching movies:', err);
+  }
+}
+
+fetchFilms('/movie/popular', '#featuredFilms');
+fetchFilms('/movie/top_rated', '#friendFilms');
