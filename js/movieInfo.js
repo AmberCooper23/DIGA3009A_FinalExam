9a1d46a6f3412c47e61e44453e86981b11fee022
AmberCooper23 <2677396@students.wits.ac.za>
2025-10-29 01:25:23 +0200

    eish

--- js/movieInfo.js
+++ js/movieInfo.js
@@ -20,8 +20,8 @@ async function loadMovie() {
         const container = document.getElementById('movieDetails');
         container.innerHTML = `
         <img src = "${IMG_BASE}${movie.poster_path}" alt="${movie.title}" class = "filmList">
-        <h1>${movie.title} (${new Date(movie.release_date).getFullYear()})</h1>
         <div class="movieInfo">
+        <h1>${movie.title} (${new Date(movie.release_date).getFullYear()})</h1>
         <p> <strong> Director: </strong> ${director}<?p>
         <p> <strong> Main Cast: </strong> ${mainCast}</p>
         <p> <strong> Rating: </strong> ${movie.vote_average}/10 </p>
