2829583c2932c05f162f9a9f51ef46ef1dfe630f
AmberCooper23 <2677396@students.wits.ac.za>
2025-11-08 05:36:25 +0200

    login fix

--- js/login.js
+++ js/login.js
@@ -63,8 +63,8 @@ signupForm.addEventListener("submit", (e) => {
 
 loginForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const username = document.getElementById("username").value.trim();
-  const password = document.getElementById("password").value.trim();
+  const username = document.getElementById("loginUsername").value.trim();
+  const password = document.getElementById("loginPassword").value.trim();
 
   let users = getUsers();
   const user = users.find(u => u.username === username && u.password === password);
